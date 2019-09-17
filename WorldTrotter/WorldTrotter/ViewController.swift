065f345857363f5b2d686eaa7227a29288120e52
Drew Burton <amburton@my.milligan.edu>
2019-09-17 08:38:26 -0400

    empty project

---
+++ WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Burton, Andrew M on 9/17/19.
+//  Copyright © 2019 Burton, Andrew M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
